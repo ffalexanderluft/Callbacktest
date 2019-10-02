c2bff5b1140c06c7d28055c9a92812d9872e7874
Alexander Luft <alexander.luft@flowfact.de>
2019-10-02 14:36:49 +0200

    ALF: app.js added

---
+++ app.js
@@ -0,0 +1,26 @@
+exports.handler =  async function(event, context) {
+    try
+    {
+    const r1 = await callapi1(false);
+    console.log("R1:",r1);
+    const r2 = await callapi1(true);
+    console.log("R2:",r2);
+    }
+    catch (e)
+    {
+        console.log(e);
+    }
+  console.log("ende");
+}
+
+async function callapi1(error){
+    return new Promise(function(resolve,reject){
+        if (error) {
+            reject("Fehler");
+        }
+        else
+        {
+          resolve('ababababa');  
+        } 
+    })
+}
